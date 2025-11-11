3913472d6b4415bd86a97d2362570c9f403e62ea
Brian Patman <Black_Reaper_Hei@yahoo.com>
2025-11-11 16:43:10 -0600

    Update accessibility_check.js
    Added some additional accessibility checks and some notes for future features.

--- accessibility_check.js
+++ accessibility_check.js
@@ -30,6 +30,65 @@
         console.log($badFrames);
     }
 
+    /*******************************************************/
+    /*   Check Lightbox Links are <button>s not <a> tags   */
+    /*******************************************************/
+    var $badLightboxLinks = [];
+    $(".lightbox-link").each(function(){
+       if($(this).is("a")){
+            $badLightboxLinks.push( $(this).get(0) );       
+       } 
+    });
+
+    if($badLightboxLinks.length > 0){
+        console.log("Found Lightbox Links that are <a> tags instead of <button> tags: ");
+        console.log($badLightboxLinks);
+    }
+
+    /*************************************************/
+    /*   Check to make sure that all popup buttons   */
+    /*      1) Are <button>s instead of <a> tags     */
+    /*      2) If they are a video popup, they       */
+    /*         should have a data-title attribute    */
+    /*         along with a data-video               */
+    /*************************************************/
+    var $badVideoPopButtons = [];
+    var $badSemanticPopButtons = [];
+    
+    $(".pop-open").each(function(){
+        if( $(this).data("video") && !$(this).data("title") ){
+            $badVideoPopButtons.push( $(this).get(0) );
+        }
+        
+        if( $(this).is("a") ){
+            $badSemanticPopButtons.push( $(this).get(0) );
+        }
+    });
+
+    if($badVideoPopButtons.length > 0 ){
+        console.log("Found Bad Video Popup Buttons: ");
+        console.log($badVideoPopButtons);
+    }
+
+    if($badSemanticPopButtons.length > 0){
+        console.log("Found Popup Buttons using <a> tags instead of <button tags: ");
+        console.log($badSemanticPopButtons);
+    }
+
+    /*************************************************************/
+    /*   Popups - Make sure all popups have correct attributes   */
+    /*************************************************************/
+
+
+    /******************************************************************/
+    /*   Links - Make sure all links on the page:                     */
+    /*     1) Don't have "here" inside them to prevent vague links    */
+    /*     2) For links that open in a new tab, make sure they have   */
+    /*        some manner of supplementary text announcing that to    */
+    /*        the user                                                */
+    /*     3)                                                         */
+    /******************************************************************/
+
     /**************************************************/
     /*   IMG - Output table of src,alt,width,height   */
     /**************************************************/
